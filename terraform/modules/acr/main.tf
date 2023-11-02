ef0d9ab1823a70f1a656b3d7235a84c68f9fa7b3
theodore-chronopoulos <el17021@mail.ntua.gr>
2023-11-02 13:23:51 +0200

    changes on naming

--- terraform/modules/acr/main.tf
+++ terraform/modules/acr/main.tf
@@ -1,5 +1,5 @@
 resource "azurerm_container_registry" "acr" {
-  name                = "containerRegistry1"
+  name                = "sirvis-acr-${var.resource_group_name}-${var.resource_group_location}"
   resource_group_name = var.resource_group_name
   location            = var.resource_group_location
   sku                 = var.acr_sku
