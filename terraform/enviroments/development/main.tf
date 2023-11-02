74e60829513b7a2191daf640f2b55d44d97b26ce
theodore-chronopoulos <el17021@mail.ntua.gr>
2023-11-02 13:15:47 +0200

    another mistake fixed

--- terraform/enviroments/development/main.tf
+++ terraform/enviroments/development/main.tf
@@ -4,6 +4,6 @@ module "resource_group" {
 
 module "acr" {
     source = "../../modules/acr"
-    resource_group_name = ["${module.resource_group.name}"]
-    resource_group_location = ["${module.resource_group.location}"]
+    resource_group_name = ["${module.resource_group.resource_group_name}"]
+    resource_group_location = ["${module.resource_group.resource_group_location}"]
 }
